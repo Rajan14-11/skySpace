12f69ef88d5ac304e81edc563c606d506f29cba0
rajan <rajangoyal125@gmail.com>
2022-07-23 17:31:22 +0530

    ordercomplete

--- src/Components/OrderComplete/OrderComplete.js
+++ src/Components/OrderComplete/OrderComplete.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import CheckoutSteps from '../Checkout/CheckoutSteps'
-
+import "./ordercomplete.css"
 function OrderComplete() {
   return (
     <>
       <CheckoutSteps activeStep={2} />
-      <div>OrderComplete</div>
+        <div className="orderSuccess">
+        <i class="fa fa-solid fa-circle-check success_icon"></i>
+
+          <p className='success_show'>Your Order has been Placed successfully </p>
+          <Link to="/orders">View Orders</Link>
+
+      </div>
     </>
   );
 }
